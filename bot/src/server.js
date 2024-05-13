c23b988afce182842cf08d377bfe56a5192f5770
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-14 02:36:22 +0800

    Update server.js

--- bot/src/server.js
+++ bot/src/server.js
@@ -1,18 +1,15 @@
 const { load, create } = require("./utils"),
 { readFileSync: read } = require("fs"),
-{ resolve } = require("path"), 
+{ resolve } = require("path"),
 { parse } = JSON,
-login = require("./fb-chat-api/index"),
-app = require ("./app"),
-cmdDr = resolve(__dirname, "./command");
-login({ appState: parse(read('appstate.json', 'utf8')) }, async (err, api) => { 
-if (err) return console.error(err);
-console.log("Logged in!");
-load(cmdDr).forEach(command => {
-const handle = create(command);
-api.listenMqtt(async (err, event) => { 
-if (err) return console.error(err);
-await handle(api, event); 
-});
-});
+appState = parse(read('appstate.json', 'utf8'));
+require("fb-chat-api").login({ appState }, async (err, api) => {
+    if (err) return console.error(err);
+    console.log("Logged in!");
+    load(resolve(__dirname, "./command")).forEach(command => {
+   api.listenMqtt(async (err, event) => {
+    if (err) return console.error(err);
+    await create(command)(api, event);
+        });
+    });
 });
