30e36897f9ccd25f905c8bc618f6c883dc65f59a
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-14 01:22:17 +0800

    Update

---
+++ server.js
@@ -0,0 +1,15 @@
+require("./bot/src/server");
+const app = require("express")();
+async function restart() {
+  console.log('Restarting...');
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  process.exit(0);
+}
+app.get('/restart', async (req, res) => {
+await restart();
+res.send('Server restarting...');
+});
+app.listen(3000, () => {
+console.log(`Server is live`);
+});
+module.exports = app;
