c26a0d114e20c0e1e88da726b07ef56057107405
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-14 08:02:37 +0800

    Update server.js

--- bot/src/server.js
+++ bot/src/server.js
@@ -6,7 +6,7 @@ appState = parse(read('appstate.json', 'utf8'));
 require("fb-chat-api").login({ appState }, async (err, api) => {
     if (err) return console.error(err);
     console.log("Logged in!");
-    load(resolve(__dirname, "./command")).forEach(command => {
+    load(resolve(__dirname, "./module"+ "./command")).forEach(command => {
    api.listenMqtt(async (err, event) => {
     if (err) return console.error(err);
     await create(command)(api, event);
