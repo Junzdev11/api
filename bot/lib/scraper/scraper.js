a74751fdc160430043098d54cd23f015e5ad0057
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-15 01:51:51 +0800

    Update scraper.js

--- bot/lib/scraper/scraper.js
+++ bot/lib/scraper/scraper.js
@@ -1,1 +1,13 @@
-.
+async function ytm(m) {
+    try {
+const search = await yt_search(m);
+   const yturl = search.videos[0].url;
+    const a = `https://tattered-lead-archaeopteryx.glitch.me/watch?v=${yturl}&audio=true`;
+        const title = search.videos[0].title;
+        const timestamp = search.videos[0].timestamp;
+        return { result: `${title} ${timestamp}`, av: a };
+    } catch (error) {
+        throw new Error("Error occurred while searching music");
+    }
+}
+module.exports = ytm;
