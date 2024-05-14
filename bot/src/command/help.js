e0f9c0a2c7ffc48a51b1c543c02850c9f04bac5f
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-14 11:44:32 +0800

    Update help.js

--- bot/src/command/help.js
+++ bot/src/command/help.js
@@ -1,9 +1,15 @@
-const { cmdName } = require('../utils');
-
+const fs = require('fs');
+const path = require('path');
+function getCommandNames(directory) {
+  const files = fs.readdirSync(directory);
+  const jsFiles = files.filter(file => file.endsWith('.js'));
+  return jsFiles.map(file => path.basename(file, '.js'));
+}
 module.exports = {
   name: "help",
   bot: async (api, event) => { 
-    const cmd = cmdName(__dirname + '/command');
+    const commandsDir = __dirname; // Same directory as the script
+    const cmd = getCommandNames(commandsDir);
     api.sendMessage("Available Commands:\n" + cmd.map((name, n) => `${n + 1}: ${name}`).join('\n'), event.threadID);
   }
 };
