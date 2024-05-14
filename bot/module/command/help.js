998a650f12d94d8d43a03463b3a21ea26b773f53
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-14 23:52:54 +0800

    Delete bot/module/command directory

--- bot/module/command/help.js
+++
@@ -1,8 +0,0 @@
-const { cmdName } = require('../../src/utils');
-module.exports = {
-name: "help",
-bot: async (api, event) => { 
-const cmd = cmdName(__dirname + "/module" + '/command');
-api.sendMessage("Available Commands:\n" + cmd.map((name, n) => `${n + 1}: ${name}`).join('\n'), event.threadID);
-}
-};
