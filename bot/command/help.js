866d859ddb31c23b30ed5a92f17461095c67b185
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-14 12:17:58 +0800

    Update help.js

--- bot/command/help.js
+++ bot/command/help.js
@@ -1,8 +1,7 @@
-const { cmdName } = require('../../utils');
+const { help } = require('../../utils');
 module.exports = {
 name: "help",
 bot: async (api, event) => { 
-const cmd = cmdName(__dirname + "/module" + '/command');
-api.sendMessage("Available Commands:\n" + cmd.map((name, n) => `${n + 1}: ${name}`).join('\n'), event.threadID);
+api.sendMessage(help(), event.threadID);
 }
 };
