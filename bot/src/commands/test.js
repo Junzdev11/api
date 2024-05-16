c2fc27a4860d0ebbe4aa55937795a507c2710efb
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-16 16:51:14 +0800

    Update test.js

--- bot/src/commands/test.js
+++ bot/src/commands/test.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "test",
-    start: async (send, name, event) => {
+     async (send, name, event) => {
       send("hi " + await name(event.senderID) + " this is a test command");
     }
 };
