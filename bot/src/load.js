5d7a970abcb508b9423bdb4b467f7bef752722af
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-16 14:53:48 +0800

    Update load.js

--- bot/src/load.js
+++ bot/src/load.js
@@ -12,4 +12,39 @@ const load = () => {
     );
 };
 
-module.exports = load;
+async function help(commandName) {
+  const commandPath = path.join(__dirname, 'commands');
+  const files = fs.readdirSync(commandPath);
+  if (commandName) {
+    for (const file of files) {
+      const filePath = path.join(commandPath, file);
+      const command = require(filePath);
+      if (command.name === commandName) {
+        return `Name: ${command.name}\nAuthor: ${command.author || ""}\nDescription: ${command.description || ""}`;
+      }
+    }
+    return `Command ${commandName} doesn't exist`;
+  } else {
+    const cmd = [];
+    files.forEach(file => {
+      const filePath = path.join(commandPath, file);
+      if (fs.lstatSync(filePath).isFile()) {
+        const filee = require(filePath);
+        if (filee && filee.name) {
+          cmd.push(filee.name);
+        }
+      }
+    });
+   let result = "Here's all available commands:\n";
+    cmd.forEach((name, i) => {
+      result += `${i + 1}. ${name}\n`;
+    });
+    return result;
+  }
+}
+
+
+module.exports = {
+ load,
+help 
+};
