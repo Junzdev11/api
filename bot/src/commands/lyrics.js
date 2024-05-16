4f011e33a9c64ebdf44b2fc4f7c59cb8cfc85412
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-16 15:36:59 +0800

    Update lyrics.js

--- bot/src/commands/lyrics.js
+++ bot/src/commands/lyrics.js
@@ -1,20 +1,19 @@
 const axios = require("axios");
-const { stream } = require ("../load");
+const { stream } = require("../load");
 module.exports = {
   name: "lyrics",
-author: "Jun",
-description: "find lyrics song",
-  start: async ( arg, send) => {
-    
+  author: "Jun",
+  description: "Find song lyrics",
+  start: async (arg, send) => {
     const srh = arg.join(' ');
     if (!srh) {
       send("Please add a title");
     } else {
       try {
-const resp = await axios.get(`https://lyrist.vercel.app/api/${srh}`);
- const { title, artist, lyrics, image } = resp.data;
+        const resp = await axios.get(`https://lyrist.vercel.app/api/${srh}`);
+        const { title, artist, lyrics, image } = resp.data;
         send({
-         body: `Title: ${title}\nArtist: ${artist}\n\n${lyrics}`,
+          body: `Title: ${title}\nArtist: ${artist}\n\n${lyrics}`,
           attachment: await stream(image),
         });
       } catch (e) {
