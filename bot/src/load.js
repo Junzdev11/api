acf37d67a6ccae31b6a6e916e141091b437408d1
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-16 12:06:30 +0800

    Update load.js

--- bot/src/load.js
+++ bot/src/load.js
@@ -1,8 +1,15 @@
-const fs = require("fs"),
-path = require("path");
-const load = () => Object.fromEntries(fs.readdirSync(path.join(__dirname, 'command')).filter(f => f.endsWith('.js')).map(f => {
-const cmd = require(`./commands/${f}`);
-return [cmd.name, cmd];
-})
-);
+const fs = require("fs");
+const path = require("path");
+
+const load = () => {
+    return Object.fromEntries(
+        fs.readdirSync(path.join(__dirname, 'commands'))
+            .filter(f => f.endsWith('.js'))
+            .map(f => {
+                const cmd = require(`./commands/${f}`);
+                return [cmd.name, cmd];
+            })
+    );
+};
+
 module.exports = load;
