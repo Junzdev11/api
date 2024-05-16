88158e6fb113ead3f456f006e9b4d110067f265a
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-16 12:03:36 +0800

    Update src

---
+++ bot/src/test.js
@@ -0,0 +1,24 @@
+const { parse } = JSON;
+const fs = require('fs');
+const path = require('path');
+const load = require("./load");
+const appState = parse(fs.readFileSync(path.join(__dirname, 'appstate.json'), 'utf8'));
+const fca = require("./fca/index");
+fca({ appState }, async (err, api) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    api.listenMqtt(async (err, event) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        const { body } = event;
+        const pr = body.split(" ")[0];
+        if (load[pr]) {
+            load[pr].start(api, event);
+        }
+    });
+});
