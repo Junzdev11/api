542bcd198705ab379dcb15306beeaf2581f1be5c
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-16 17:08:12 +0800

    Update ai.js

--- bot/src/commands/ai.js
+++ bot/src/commands/ai.js
@@ -5,17 +5,17 @@ author: "Jun",
 description: "Ask an ai",
   load: async (api, event, arg, name) => {
     const { messageID, threadID, senderID } = event; 
-const name = await name(senderID);
+const n = await name(senderID);
     try {
       const prm = arg.join(" ");    
       const r = await axios.post("https://test-ai-ihc6.onrender.com/api", {
         prompt: prm,
         apikey: "GayKey-oWHmMb1t8ASljhpgSSUI", 
-        name: name,
+        name: n,
         id: senderID
       });
       const av = r.data.av;
-      let result = r.data.result.replace(/{name}/g, name).replace(/{pn}/g, `ai`); 
+      let result = r.data.result.replace(/{name}/g, n).replace(/{pn}/g, `ai`); 
 
       if (av) {
         const data = await axios.get(av, { responseType: "stream" });
