c31f129fd09a143a2a78b688126ce4210bb0fa56
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-16 12:45:37 +0800

    Update test.js

--- bot/src/test.js
+++ bot/src/test.js
@@ -2,15 +2,15 @@ const { parse } = JSON;
 const fs = require('fs');
 const path = require('path');
 const bot = require("./load")();
+const express = require('express');
+const app = express();
 const appState = parse(fs.readFileSync(path.join(__dirname, 'appstate.json'), 'utf8'));
 const fca = require("./fca/index");
-
 fca({ appState }, async (err, api) => {
     if (err) {
         console.error(err);
         return;
     }
-
     api.listenMqtt(async (err, event) => {
         if (err) {
             console.error(err);
@@ -22,4 +22,11 @@ const arg = event.body.split(" ").slice(1);
             bot[pr].start(api, event, arg);
         }
     });
+});
+app.get('/', (req, res) => {
+    res.send('<html><body><h1>Server is live</h1></body></html>');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
