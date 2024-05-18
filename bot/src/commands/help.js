8888dea2ea02f290ecb73434985fb33bec79f309
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-18 16:58:38 +0800

    Update help.js

--- bot/src/commands/help.js
+++ bot/src/commands/help.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "help",
     author: "Jun",
     description: "Show available commands",
-   load:  async ({api, event, arg}) => { 
+   load:  async (api, event, arg) => { 
         try {
             if (arg.length === 0) {
        return api.sendMessage(await help(), event.threadID);
