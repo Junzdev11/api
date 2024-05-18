c0d85f859551be1668191bb15ca18d8fa2f9c0b4
Junzdev11 <111303157+Junzdev11@users.noreply.github.com>
2024-05-18 14:49:08 +0800

    Update pinterest.js

--- bot/src/commands/pinterest.js
+++ bot/src/commands/pinterest.js
@@ -4,13 +4,12 @@ module.exports = {
     name: "pinterest",
     author: "Jun",
     description: "Find image on Pinterest",
-    load: async ({ event, api }) => {
-        const { threadID, messageID, args } = event;
-        const amount = parseInt(args[1]);
-        const search = args.slice(2).join(" ");
-
+    load: async ({ event, api, arg }) => {
+        const { threadID, messageID } = event;
+        const amount = parseInt(arg[1]);
+        const search = arg.slice(2).join(" ");
         if (!search || !amount || isNaN(amount)) {
-            return api.sendMessage(
+      return api.sendMessage(
                 "Invalid usage. Please use:\n" +
                 `${p}pinterest [amount 1-9] [query]\n` +
                 "Example:\n" +
@@ -19,7 +18,7 @@ module.exports = {
             if (amount > 9) {
                 return api.sendMessage("Sorry, the maximum amount is 9 only", threadID, messageID);
             } else {
-                const images = await searchPinterest({ search, amount });
+                const images = await pinterest({ search, amount });
                 api.sendMessage({
                     body: search,
                     attachment: images
